0ff8bb694f958dc001d828de66163a0a98a0b3c8
Dasari Rajnarayana <drajnaryana@gmail.com>
2022-03-07 16:24:04 +0530

    updated

--- index.js
+++ index.js
@@ -9,18 +9,23 @@ clientSecret="eafd7c01ccab5ad51914556357688a269ed8f382"
 //API request
 
 
-// var requestOptions = {
-//     method: "GET",
-//     headers: {
-//       Authorization: `Client-ID ${clientId}`,
-//     },
-//     redirect: "follow",
-//   };
-  
-// fetch("https://api.imgur.com/3/gallery/t/pokemon", requestOptions)
-//   .then(response => response.json())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
+var requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Client-ID ${clientId}`,
+    },
+    redirect: "follow",
+  };
+
+  let tags=[]
+fetch("https://api.imgur.com/3/tags", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    tags = [...result.data.tags];
+    console.log("tags:", tags);
+
+  })
+  .catch(error => console.log('error', error));
 
 
 // For more Tags displaying the block
@@ -70,24 +75,91 @@ function myFunction() {
 }
 
 
-document.querySelector("#return-to-top").addEventListener("click", scrollTop);
+// document.querySelector("#return-to-top").addEventListener("click", scrollTop);
 
-function scrollTop() {
-  console.log("Yes")
-  // document.body.scrollTop = 0;
-  // document.documentElement.scrollTop = 0;
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-}
+// function scrollTop() {
+//   console.log("Yes")
+//   // document.body.scrollTop = 0;
+//   // document.documentElement.scrollTop = 0;
+//   window.scrollTo({
+//     top: 0,
+//     left: 0,
+//     behavior: "smooth",
+//   });
+// }
 
 function debounce(){
   let input=document.getElementById("input").value;
-  let name1=document.getElementsByClassName("name_1")
-  for(var i=0;i<name1.length;i++){
-    name1[i].textContent=`${input}`
+  let showSearch=document.getElementById("showSearchBar")
+  if(!input){
+    showSearch.classList.remove("searchBar")
+
+    showSearch.classList.add("searchBarhide")
   }
+  else{
+    showSearch.classList.remove("searchBarhide")
+
+    showSearch.classList.add("searchBar")
+  }
+
+  let name1=document.getElementsByClassName("name_1")
+  let name2=document.getElementsByClassName("name_2")
+  // for(var i=0;i<name1.length;i++){
+  //   name1[i].textContent=`${input}`
+  // }
+  let result=tags.filter((item)=>{
+    return item.name.startsWith(`${input}`)
+
+  })
+  if(result.length!==0){
+    for(var i=0;i<name1.length;i++){
+      if(!result[i]){
+       name1[i].textContent=`${result[0].name}`
+      }
+      else{
+       name1[i].textContent=`${result[i].name}`
+      }
+     
+   }
+   for(var i=0;i<name2.length;i++){
+     if(!result[i]){
+      name2[i].textContent=`${result[0].name}`
+     }
+     else{
+      name2[i].textContent=`${result[i].name}`
+     }
+    
+  }
+  }
+  else{
+    for(var i=0;i<3;i++){
+     name1[i].textContent="nothing found"
+     name2[i].textContent="nothing found"
+   }
+  }
+
+
+ 
+ 
   
 }
+
+let name2=document.getElementsByClassName("name_2")
+// console.log("name2",name2)
+for(let i=0;i<name2.length;i++){
+  // console.log(name2[i])
+  name2[i].addEventListener("click",()=>{
+    let tagName=name2[i].textContent
+
+    fetch(`https://api.imgur.com/3/gallery/t/${tagName}`, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    // tags = [...result.data.tags];
+    console.log("tags:",result);
+
+  })
+  .catch(error => console.log('error', error));
+  })
+}
+
+
